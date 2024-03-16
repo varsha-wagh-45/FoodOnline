f7978e2808b53521c35cf0864528965b9cfe600f
varsha <varshawagh450@gmail.com>
2024-03-16 11:15:57 +0530

    cart functionality

--- accounts/views.py
+++ accounts/views.py
@@ -12,7 +12,7 @@ from django.contrib.auth.decorators import login_required,user_passes_test
 from django.core.exceptions import PermissionDenied
 from django.contrib.auth.tokens import default_token_generator
 from django.utils.http import  urlsafe_base64_decode 
-    
+from django.template.defaultfilters import  slugify
 
 #restrict the vendor from accessing the customer page
 def check_role_vendor(user):
@@ -75,7 +75,7 @@ def registerUser(request):
 def registerVendor(request):
     if request.user.is_authenticated:
         messages.warning(request, 'You are already logged in!')
-        return redirect('dashboard')
+        return redirect('myAccount')
     elif request.method=='POST':
        #store the data and create the user
       form = UserForm(request.POST)
@@ -91,6 +91,8 @@ def registerVendor(request):
             user.save()
             vendor = v_form.save(commit=False)
             vendor.user = user
+            vendor_name=v_form.cleaned_data['vendor_name']
+            vendor.vendor_slug = slugify(vendor_name)+'-'+str(user.id)
             user_profile = UserProfile.objects.get(user=user)
             vendor.user_profile = user_profile
             vendor.save()
---
+++ marketplace/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+
+from marketplace.models import Cart
+
+class CartAdmin(admin.ModelAdmin):
+    list_display = ('user', 'fooditem', 'quantity', 'updated_at')
+
+
+
+
+
+
+
+
+
+
+# Register your models here.
+admin.site.register(Cart,CartAdmin)
